fc0870c78ddd4f4b3ca44d0335529de5f844098f
TomNowa810 <130317513+TomNowa810@users.noreply.github.com>
2023-04-10 00:58:27 +0200

    Complete openapi integration

---
+++ build/openapi/models/UserDto.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Return Object of an User
+ */
+export type UserDto = {
+    id?: number;
+    name?: string;
+    totalRuns?: number;
+};
