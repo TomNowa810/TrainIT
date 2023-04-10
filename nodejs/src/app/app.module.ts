9e0b99ac44d463e0b3dd05249002be36abd6f556
TomNowa810 <130317513+TomNowa810@users.noreply.github.com>
2023-04-10 18:16:06 +0200

    restructure

--- nodejs/src/app/app.module.ts
+++ nodejs/src/app/app.module.ts
@@ -1,20 +1,43 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OpenAPI } from 'build/openapi';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    InputTextModule,
+    CheckboxModule,
+    RadioButtonModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ButtonModule,
+    PasswordModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide : APP_INITIALIZER,
+      useFactory: () => {
+        
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- nodejs/src/app/login/login.component.ts
+++ nodejs/src/app/login/login.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-    variable: string = 'This is my Message luli';
+    
+    _passwordValue: string | null | undefined;
+    _usernameValue: string | null | undefined;
+
+    _output: boolean = false;
+    _loginPassed: boolean = false;
+
+    async _clickEvent() {
+      const userNameFinal: string = "Thomas";
+      const passwordFinal: string = "MyPassword12";
+
+      if(this._usernameValue == userNameFinal && 
+        this._passwordValue == passwordFinal){
+          this._output = true;
+          this._loginPassed = true;
+        } else {
+          this._usernameValue = null;
+          this._passwordValue = null;
+        }
+    }
 }
