eb577faa2157f1786eb595f7b88f9c14c5dfae41
TomNowa810 <130317513+TomNowa810@users.noreply.github.com>
2023-05-04 23:14:19 +0200

    expand IntegrationTests

--- java/src/main/test/java/tomNowa/trainIT/be/CalculationServiceIntegrationTest.java
+++ java/src/main/test/java/tomNowa/trainIT/be/CalculationServiceIntegrationTest.java
@@ -1,7 +1,9 @@
 package tomNowa.trainIT.be;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tomNowa.trainIT.be.model.Run;
@@ -13,12 +15,13 @@ import tomNowa.trainIT.be.service.CalculationService;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
-public class CalculationServiceIntegrationTest extends IntegrationTestSetup{
+class CalculationServiceIntegrationTest extends IntegrationTestSetup{
 
     @Autowired
     private RunRepository repository;
@@ -33,20 +36,31 @@ public class CalculationServiceIntegrationTest extends IntegrationTestSetup{
         setUpRuns();
     }
 
-    @Test
-    void testCreateCalculationByTimerange_lastWeek(){
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void testCreateCalculationByTimerange(final Timerange timerange, final Integer totalRuns,
+                                                   final Double avgKm, final Integer avgSecondsKm){
+
         final RunCalculationDto expectedCalculation = new RunCalculationDto();
-        expectedCalculation.setTotalRuns(3);
-        expectedCalculation.setAvgKm(BigDecimal.valueOf(5.73));
-        expectedCalculation.setAvgSecondsPerKm(319);
+        expectedCalculation.setTotalRuns(totalRuns);
+        expectedCalculation.setAvgKm(BigDecimal.valueOf(avgKm));
+        expectedCalculation.setAvgSecondsPerKm(avgSecondsKm);
 
-        final RunCalculationDto returnedCalculation = sut.createCalculationByTimerange(USER_ID, Timerange.LAST_WEEK);
-
+        final RunCalculationDto returnedCalculation = sut.createCalculationByTimerange(USER_ID, timerange);
+        System.out.println(returnedCalculation);
         assertThat(returnedCalculation).isNotNull();
-        assertThat(returnedCalculation.getTotalRuns()).isEqualTo(3);
-        assertThat(returnedCalculation.getAvgKm()).isEqualTo(expectedCalculation.getAvgKm());
-        assertThat(returnedCalculation.getAvgSecondsPerKm()).isEqualTo(expectedCalculation.getAvgSecondsPerKm());
         assertThat(returnedCalculation).isEqualTo(expectedCalculation);
+    }
+
+    private static Stream<Arguments> provideTestData(){
+        return Stream.of(
+                Arguments.of(Timerange.LAST_WEEK, 3, 5.73d, 319),
+                Arguments.of(Timerange.LAST_TWO_WEEKS, 5, 5.38d, 337),
+                Arguments.of(Timerange.LAST_MONTH, 12, 5.06d, 349),
+                Arguments.of(Timerange.LAST_QUARTER, 13, 5.08d, 351),
+                Arguments.of(Timerange.HALF_YEAR, 13, 5.08d, 351),
+                Arguments.of(Timerange.LAST_YEAR, 15, 6.06d, 359)
+        );
     }
 
     private void setUpRuns(){
@@ -55,7 +69,8 @@ public class CalculationServiceIntegrationTest extends IntegrationTestSetup{
         repository.saveAndFlush(new Run(7,2,4.48,1630, setUpDate(24)));
         repository.saveAndFlush(new Run(8,2,4.65,1680, setUpDate(23)));
         repository.saveAndFlush(new Run(9,2,4.58,1685, setUpDate(20)));
-        repository.saveAndFlush(new Run(11,2,5.78,1960, setUpDate(19)));
+        repository.saveAndFlush(new Run(10,2,4.88,1715, setUpDate(29)));
+        repository.saveAndFlush(new Run(11,2,5.78,1960, setUpDate(17)));
         repository.saveAndFlush(new Run(12,3,9.75,3630, setUpDate(16)));
         repository.saveAndFlush(new Run(13,3,7.9,2570, setUpDate(13)));
         repository.saveAndFlush(new Run(14,2,4.75,1730, setUpDate(12)));
@@ -64,6 +79,8 @@ public class CalculationServiceIntegrationTest extends IntegrationTestSetup{
         repository.saveAndFlush(new Run(17,2,6.30,1960, setUpDate(5)));
         repository.saveAndFlush(new Run(18,2,4.05,1430, setUpDate(3)));
         repository.saveAndFlush(new Run(19,2,6.85,2023, setUpDate(1)));
+        repository.saveAndFlush(new Run(20,2,5.32,1980, setUpDate(60)));
+        repository.saveAndFlush(new Run(21,2,9.75,3630, setUpDate(200)));
     }
 
     private Date setUpDate(final int minusDays){
