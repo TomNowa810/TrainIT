0c80cea7ffe9573cee1b90c7bd0f491c4287840b
TomNowa810 <130317513+TomNowa810@users.noreply.github.com>
2023-05-20 23:59:01 +0200

    fix query bug

--- java/src/main/java/tomNowa/trainIT/be/repository/RunRepository.java
+++ java/src/main/java/tomNowa/trainIT/be/repository/RunRepository.java
@@ -2,7 +2,6 @@ package tomNowa.trainIT.be.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tomNowa.trainIT.be.model.Run;
 
@@ -19,7 +18,6 @@ public interface RunRepository extends JpaRepository<Run, Integer> {
     @Query("SELECT r FROM Run r WHERE r.userId = :userId AND r.date <= :fromDate AND r.date >= :toDate")
     List<Run> runsInTimerange(final int userId, final Date fromDate, final Date toDate);
 
-   // @Query("SELECT new tomNowa.trainIT.be.model.Run(run_id, user_id, km_number, seconds, date) " +
-   //         "FROM Run WHERE user_id = :userId ORDER BY date DESC LIMIT :numberOfLastRuns")
-   // List<Run> lastRuns(final int userId, final int numberOfLastRuns);
+    @Query(value = "SELECT * FROM runs WHERE user_id = :userId ORDER BY date DESC LIMIT :numberOfLastRuns", nativeQuery = true)
+    List<Run> lastRuns(final int userId, final int numberOfLastRuns);
 }
